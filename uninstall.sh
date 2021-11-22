4b9c556a88ceccd8a7374404e7295ec16216ef38
Mentor <94018962+Gasstrum@users.noreply.github.com>
2021-11-22 20:08:46 +0800

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -1,19 +1,3 @@
-#!/bin/bash
-
-echo '============================================='
-echo '          Sila Tunggu sebentar '
-echo 'Process Uninstall Sedang Dijalankan '
-echo '============================================='
-sleep 2
-rm -rf install.sh
-rm -rf install.sh.1
-rm -rf install.sh.2
-rm -rf install.sh.3
-clear
-echo '============================================='
-echo '          Sila Tunggu sebentar '
-echo 'Process Update & Upgrade Sedang Dijalankan '
-echo '============================================='
-sleep 2
-apt update && apt upgrade -y
-clear
+z="
+";qz='nkan';dz='h.1';Dz='==='\''';Nz='oces';oz='g Di';Xz='p 2';hz='r';Lz=' '\''';mz='de S';Bz=' '\''==';Cz='====';Iz='ggu ';Vz='n '\''';rz='apt ';nz='edan';Gz='Sila';wz='grad';fz='h.3';tz='te &';Tz='ijal';sz='upda';ez='h.2';Kz='ntar';lz='pgra';Yz='rm -';cz='h';Hz=' Tun';kz=' & U';az='nsta';gz='clea';Ez=' '\''  ';vz='t up';Oz='s Un';Wz='slee';xz='e -y';Jz='sebe';Rz='Seda';iz='s Up';Qz='all ';Uz='anka';uz='& ap';bz='ll.s';Fz='    ';Az='echo';Zz='rf i';pz='jala';Sz='ng D';Pz='inst';jz='date';Mz=' '\''Pr';
+eval "$Az$Bz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Dz$z$Az$Ez$Fz$Fz$Gz$Hz$Iz$Jz$Kz$Lz$z$Az$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$z$Az$Bz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Dz$z$Wz$Xz$z$Yz$Zz$az$bz$cz$z$Yz$Zz$az$bz$dz$z$Yz$Zz$az$bz$ez$z$Yz$Zz$az$bz$fz$z$gz$hz$z$Az$Bz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Dz$z$Az$Ez$Fz$Fz$Gz$Hz$Iz$Jz$Kz$Lz$z$Az$Mz$Nz$iz$jz$kz$lz$mz$nz$oz$pz$qz$Lz$z$Az$Bz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Cz$Dz$z$Wz$Xz$z$rz$sz$tz$uz$vz$wz$xz$z$gz$hz"
